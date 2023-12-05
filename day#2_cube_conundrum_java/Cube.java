e6edd37859a2c8a4f15557f310f3b8e2e13579db
Eric Foote <ericfoote2014@hotmail.com>
2023-12-05 17:03:03 -0400

    Stuck on part#1

--- day#2_cube_conundrum_java/Cube.java
+++ day#2_cube_conundrum_java/Cube.java
@@ -57,9 +57,6 @@ import java.util.Scanner;
             System.out.println("Error");
             e.printStackTrace();
         }
-        for (int i = 0; i < input.size(); i++) {
-            System.out.println(input.get(i));
-        }
         
     }
 }
