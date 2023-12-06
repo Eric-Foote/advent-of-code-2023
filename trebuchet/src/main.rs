a279cfed7b470cb198ec6d6f141716f54d1d71dc
Eric Foote <ericfoote2014@hotmail.com>
2023-12-06 11:48:12 -0400

    Using the rust documentation I have proceeded a little bit further on

--- trebuchet/src/main.rs
+++ trebuchet/src/main.rs
@@ -1,3 +1,19 @@
+// Files have to be in the debug folder
+
+// cargo run has to be used in the debug folder
+
+use std::fs::read_to_string;
+
 fn main() {
-    println!("Hello, world!");
+
+    let mut result = Vec::new();
+
+    for line in read_to_string("Testing.txt").unwrap().lines(){
+        result.push(line.to_string())
+    }
+
+    println!("{:#?}", result);
+
+
+
 }
