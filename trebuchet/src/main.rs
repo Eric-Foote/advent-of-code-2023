fbb6d09d04ace3351b155b0002c26d7c1aa42176
Eric Foote <ericfoote2014@hotmail.com>
2023-12-06 23:32:43 -0400

    Got part 1 working with a lot of googling and rust documentation and rust forums

--- trebuchet/src/main.rs
+++ trebuchet/src/main.rs
@@ -5,6 +5,9 @@
 use std::fs::read_to_string;
 
 fn main() {
+    let mut left = '0';
+    let mut right = '0';
+    let mut sum: u32 = 0;
 
     let mut result = Vec::new();
 
@@ -14,6 +17,35 @@ fn main() {
 
     println!("{:#?}", result);
 
+    for (i,_x) in result.iter().enumerate(){
+        let resultant = &result[i];
+        let chars: Vec<char> = resultant.chars().collect();
+        for c in &chars {
+            if c.is_numeric() {
+                left = *c;
+                //println!("{left}");
+                break;
+            }
+        }
+        for c in chars.iter().rev(){
+            if c.is_numeric() {
+                right = *c;
+                //println!("{right}");
+                break;
+            }
 
+        }
+
+        let mut intermediate = String::from("");
+        intermediate.push_str(&left.to_string());
+        intermediate.push_str(&right.to_string());
+        println!("{intermediate}");
+
+
+        let intermediate_value: u32 = intermediate.trim().parse().unwrap();
+        sum = sum + intermediate_value;
+
+    }
+    println!("{sum}");
 
 }
