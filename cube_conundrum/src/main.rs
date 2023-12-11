b62356878693d5fe25f535feb71d077f7f1498cf
Eric Foote <ericfoote2014@hotmail.com>
2023-12-10 21:57:43 -0400

    Giving up and moving on

--- cube_conundrum/src/main.rs
+++ cube_conundrum/src/main.rs
@@ -1,3 +1,32 @@
+use std::fs::read_to_string;
+use regex::Regex;
+
 fn main() {
-    println!("Hello, world!");
-}
+
+    const max_red: u32 = 12;
+    const max_green: u32 = 13;
+    const max_blue: u32 = 14;
+    let mut result = Vec::new();
+
+    for line in read_to_string("resources/Testing.txt").unwrap().lines(){
+        result.push(line.to_string())
+    }
+
+    println!("{:#?}", result);
+
+    for (game_id,game) in result.iter().enumerate() {
+        let subgame = game.split(";");
+        let subgame = subgame.collect::<Vec<_>>();
+        for (i,subgames) in subgame.iter().enumerate(){
+            let subgames = subgames.replace("Game","");
+            let colour = subgames.split(", ");
+            for c in colour {
+                let re = Regex::new(r" \d:").unwrap();
+                let c = &re.replace_all(c, "");
+                println!{"{:#?}", c}
+
+                }
+            }
+        }
+    }
+
